097002444f13279bae7c7d24f3a49d3469547cac
brian <brianferrol@hotmail.com>
2021-03-20 19:15:03 +0100

    Added Grade, deadline and last_update to the columns.

--- app/international_organizations.py
+++ app/international_organizations.py
@@ -25,13 +25,24 @@ class InternationalOrganizationsScrapper:
         self.job_list["job_url"] = self.job_list["job_url"].apply(lambda x: self._make_hyperlink(x))
 
         # order DataFrame columns (TOC):
-        self.job_list = self.job_list[["title", "job_url"]]
+        self.job_list = self.job_list[["title", "job_url", "grade", "last_update", "deadline"]]
 
         # save DataFrame to excel.
         self.job_list.to_excel(path + filename, encoding="utf-8", index=False)
         return print("job list saved on {}".format(path + filename))
 
     def OSCE(self):
+
+        def _get_metadata_from_job(job_url):
+            soup = BaseScrapper(job_url).scrap()
+            body = soup.findAll("section", attrs={"class": "content-metadata"})
+
+            metadata = {}
+            for i in range(0, len(body[0].findAll("dd"))):
+                metadata[body[0].findAll("dt")[i].text] = body[0].findAll("dd")[i].text.replace("\n", "").strip()
+
+            return metadata
+
         url = "https://jobs.osce.org/vacancies?f%5B0%5D=duty_station%3AVienna"
         soup = BaseScrapper(url=url).scrap()
         results = soup.find(id='block-mainpagecontent')
@@ -39,10 +50,20 @@ class InternationalOrganizationsScrapper:
         job_elems = results.find_all('div', attrs={'class': 'teaser__content'})
 
         for job in job_elems:
-            job_url = job.find("a", href=True)["href"]
+            job_url = "https://jobs.osce.org" + job.find("a", href=True)["href"]
             title = job.find("a", href=True).text
+            metadata = _get_metadata_from_job(job_url)
+
+            last_update = metadata["Date of issue"]
+            closing_date = metadata["Deadline for application"]
+            try:
+                grade = metadata["Grade"]
+            except KeyError:
+                grade = None
+
             self.job_list = self.job_list.append(
-                {"title": title, "job_url": "https://jobs.osce.org" + job_url},
+                {"title": title, "job_url": job_url, "grade": grade,
+                 "last_update": last_update, "deadline": closing_date},
                 ignore_index=True)
 
     def UN_jobs(self):
@@ -50,6 +71,20 @@ class InternationalOrganizationsScrapper:
         This website presents a particular difficulty: as there is too many jobs, they are split in different
         pages.
         """
+
+        def _get_closing_date(job_url):
+            soup = BaseScrapper(job_url).scrap()
+            ps = soup.findAll("p")
+            for p in ps:
+                if p.find("strong", text="Deadline for Applications:"):
+                    return p.text.split(": ")[1]
+
+        def _get_grade(job_url):
+            soup = BaseScrapper(job_url).scrap()
+            a_list = soup.findAll("li", attrs={"class": "list-group-item dropdown-toggle"})
+            for a in a_list:
+                if a.text.find("Grade:") >= 0:
+                    return a.text.split(":")[1]
 
         # Get jobs from first page:
         url = "https://unjobs.org/duty_stations/vie"
@@ -60,8 +95,12 @@ class InternationalOrganizationsScrapper:
                 id = job["id"]  # force error if not id
                 job_url = job.find("a", href=True)["href"]
                 title = job.find("a", href=True).text
+                last_update = job.find("time").text
+                closing_date = _get_closing_date(job_url)
+                grade = _get_grade(job_url)
                 self.job_list = self.job_list.append(
-                    {"title": title, "job_url": job_url},
+                    {"title": title, "job_url": job_url, "grade": grade,
+                     "last_update": last_update, "deadline": closing_date},
                     ignore_index=True)
             except KeyError:
                 pass
--- test/web-srapping-basics.py
+++ test/web-srapping-basics.py
@@ -1,15 +1,37 @@
+import re
+
 import requests
 from bs4 import BeautifulSoup
 
-url = "https://jobs.osce.org/vacancies?f%5B0%5D=duty_station%3AVienna"
+url = "https://jobs.osce.org/vacancies/senior-auditing-assistant-vnsecg01609"
 page = requests.get(url)
 
 soup = BeautifulSoup(page.content, 'html.parser')
-results = soup.find(id='block-mainpagecontent')
 
-job_elems = results.find_all('div', attrs={'class': 'teaser__content'})
+ps = soup.findAll("p")
+for p in ps:
+    if p.find("strong", text="Deadline for Applications:"):
+        print(p.text.split(": ")[1])
 
-for job in job_elems:
-    job_url = job.find("a", href=True)["href"]
-    title = job.find("a", href=True).text
-    print(title, job_url)
+a_list = soup.findAll("li", attrs={"class": "list-group-item dropdown-toggle"})
+for a in a_list:
+    if a.text.find("Grade:") >= 0:
+        print(a.text.split(":")[1])
+
+cd = soup.findAll("div")
+for div in cd:
+    try:
+        id = div["id"]
+        print(div.text)
+    except KeyError:
+        pass
+
+body = soup.findAll("section", attrs={"class": "content-metadata"})
+dds = body[0].findAll("dd")
+grade = body[0].getText().split("Grade ")[1].split("\n")[0]
+for dd in dds:
+    print(dd.text)
+
+a= {}
+for i in range(0, len(body[0].findAll("dd"))):
+    a[body[0].findAll("dt")[i].text] = body[0].findAll("dd")[i].text.replace("\n", "")
